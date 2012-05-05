e8862c0c000bd843577219506a56db0a1f10e662
rod <rod@pu-gh.com>
2012-05-05 09:31:26 +0100

    added marginalia

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
-(defproject bayes "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+
+(defproject bayes "0.0.1"
+  :description "Simple naive baysian analysis"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [stout "0.1.0"]]
+  :dev-dependencies [[lein-marginalia "0.7.0"]])
+
